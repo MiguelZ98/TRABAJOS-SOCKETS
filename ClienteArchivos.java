d2d659bcaf22ea32a3c957ae57866fc3fa100c89
MiguelZ98 <55512019+MiguelZ98@users.noreply.github.com>
2019-09-18 14:37:14 -0600

    Add files via upload

---
+++ ClienteArchivos.java
@@ -0,0 +1,10 @@
+
+package clientearchivos;
+
+public class ClienteArchivos {
+
+    public static void main(String[] args) {
+        Cliente Cl = new Cliente(args);
+        Cl.Iniciar();
+    }
+}
---
+++ ServidorArchivos.java
@@ -0,0 +1,9 @@
+package servidorarchivos;
+
+public class ServidorArchivos {
+
+    public static void main(String[] args) {
+        Servidor SV = new Servidor(args);
+        SV.Iniciar();
+    }
+}
